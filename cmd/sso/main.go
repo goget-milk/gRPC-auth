c162b96d6e5d246f1058b5fa6994c880f085dce0
goget-milk <mdevmann@yahoo.com>
2024-12-19 14:43:58 +0300

    init and setup logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,14 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"github.com/goget-milk/sso/internal/config"
+	"github.com/goget-milk/sso/internal/lib/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
-	// TODO: Init logger
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.Any("config", cfg),
+	)
+
 	// TODO: Init application (app)
 	// TODO: Run applications gRPC-server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+		//log = slog.New(
+		//	slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		//)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
